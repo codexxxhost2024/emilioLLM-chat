00e414ed41e821bf2ddd164eb3b9e850aa4abbf7
MasterDee-Explore <codexxxhost@gmail.com>
2024-11-26 15:37:09 +0800

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,14 +1,14 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">Hello, I am MasterE</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      For inquiries or to create your very own AI assistant, 
+      Contact Master E on WhatsApp: +63 905 674 1316.
+      Let’s transform the way you work!
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
+      <a className="underline" href="https://github.com/codexxxhost2024">
+        Medical Assistant
       </a>
     </p>
   </div>
